4d8b7a34493375a40052ed6f5798bd34c46ca8ba
Tiger Feihu Chen <sydneytiger@gmail.com>
2018-03-30 18:19:55 +1100

    added FirstPage component

---
+++ src/components/FirstPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="first-page">
+    <h1>Here is my first Vue component</h1>
+    <div>{{msg}} - {{title}}</div>
+    <div>The number of click is: {{numberOfClick}}
+      <button v-on:click="addCount">Add one</button>
+      <button v-on:click="resetCount">Reset</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstPage',
+  data() {
+    return {
+      msg: 'Welcome to my first Vue component page.',
+      numberOfClick: 0,
+    };
+  },
+  methods: {
+    addCount() {
+      this.numberOfClick += 1;
+    },
+
+    resetCount() {
+      this.numberOfClick = 0;
+    },
+  },
+  props: ['title'],
+};
+</script>
+
+<style scoped>
+
+</style>
